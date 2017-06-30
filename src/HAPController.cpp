f0f5a915fd483c571ce5a8a6f295fdc04a3c2f39
mobileflowllc <fieldforceapp@gmail.com>
2017-06-30 12:35:01 -0700

    link error

---
+++ src/HAPController.cpp
@@ -0,0 +1,32 @@
+//
+//  HAPController.cpp
+//  homekit-wolfssl
+//
+//  Created by d. nye on 6/30/17.
+//  Copyright © 2017 Mobile Flow LLC. All rights reserved.
+//
+
+#include "HAPController.h"
+
+
+int
+getChallenge(uint8_t **salt,uint16_t *salt_len, uint8_t **key, uint16_t *key_len)
+{
+//    Srp srp;
+//
+//    wc_SrpInit(&srp, SRP_TYPE_SHA, SRP_CLIENT_SIDE);
+//    
+//    wc_SrpTerm(&srp);
+    
+    return 0;
+    
+}
+
+
+int
+verifySession(uint8_t **serverKeyProof, uint16_t *proof_len, uint8_t *clientPublicKey, uint16_t client_key_len, uint8_t *clientKeyProof, uint16_t client_proof_len)
+{
+
+    return 0;
+    
+}
---
+++ src/HAPController.h
@@ -0,0 +1,25 @@
+//
+//  HAPController.hpp
+//  homekit-wolfssl
+//
+//  Created by d. nye on 6/30/17.
+//  Copyright © 2017 Mobile Flow LLC. All rights reserved.
+//
+
+#ifndef HAPController_H
+#define HAPController_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#define WOLFCRYPT_HAVE_SRP 1
+#define WOLFSSL_SHA512 1
+
+#include "srp.h"
+
+int getChallenge(uint8_t **salt,uint16_t *salt_len, uint8_t **key, uint16_t *key_len);
+
+int verifySession(uint8_t **serverKeyProof, uint16_t *proof_len, uint8_t *clientPublicKey, uint16_t client_key_len, uint8_t *clientKeyProof, uint16_t client_proof_len);
+
+
+#endif /* HAPController_hpp */
